07785d55e0b980b154d60326bbff39b72d03b854
IsayaOsward <isaya.nyamavindi@eganet.go.tz>
2024-11-28 12:52:35 +0300

    fixed phone numbers and theme

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,8 +1,8 @@
 class Routes {
-  static const String welcome = '/';
+  static const String welcome = '/welcome';
   static const String login = "/login";
   static const String register = '/register';
-  static const String guest = '/guest';
+  static const String guest = '/';
   static const String contactUs = '/contact_us';
   static const String profile = "/profile";
   static const String homepage = "/homepage";
